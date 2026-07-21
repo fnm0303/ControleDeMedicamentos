558e03df148047d1b4828847693bd0d1ab11a6e3
Felipe Neto Mello <fnm03br@gmail.com>
2026-07-21 20:19:57 -0300

    Adiciona Listagem de Fornecedores

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/FornecedorController.cs
@@ -0,0 +1,21 @@
+using ControleDeMedicamentos.ConsoleApp.Compartilhado.Arquivos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
+
+public sealed class FornecedorController : Controller
+{
+    private readonly RepositorioFornecedorEmArquivo repositorio;
+    public FornecedorController()
+    {
+        ContextoJson contexto = new ContextoJson();
+        contexto.Carregar();
+        repositorio = new RepositorioFornecedorEmArquivo(contexto);
+    }
+    [HttpGet] //método será acessado pela barra de endereços do navegador
+    public ActionResult Listar()
+    {
+        List<Fornecedor> fornecedores = repositorio.SelecionarTodos();
+        return View(fornecedores); //retorna uma página View
+    }
+}
