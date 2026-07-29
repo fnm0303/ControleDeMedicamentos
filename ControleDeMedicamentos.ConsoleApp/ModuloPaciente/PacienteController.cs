af9aff2afaf6e5147b06e94507567c384239399f
Felipe Neto Mello <fnm03br@gmail.com>
2026-07-29 18:59:56 -0300

    Adicionado Editar Paciente

--- ControleDeMedicamentos.ConsoleApp/ModuloPaciente/PacienteController.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPaciente/PacienteController.cs
@@ -30,4 +30,46 @@ public sealed class PacienteController : Controller
         return View(viewModels);
     }
 
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarPacienteViewModel cadastrarVm)
+    {
+        Paciente paciente = new Paciente(cadastrarVm.Nome, cadastrarVm.Telefone, cadastrarVm.CartaoSus, cadastrarVm.CPF);
+
+        repositorioPaciente.Cadastrar(paciente);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(int id)
+    {
+        Paciente? pacienteSelecionado = repositorioPaciente.SelecionarPorId(id);
+
+        if (pacienteSelecionado == null)
+            return NotFound();
+
+        EditarPacienteViewModel vm = new EditarPacienteViewModel(id, pacienteSelecionado.Nome, pacienteSelecionado.Telefone, pacienteSelecionado.CartaoSus, pacienteSelecionado.CPF);
+
+        return View(vm);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(EditarPacienteViewModel editarVm)
+    {
+        Paciente pacienteAtualizado = new Paciente(editarVm.Nome, editarVm.Telefone, editarVm.CartaoSus, editarVm.CPF);
+
+        bool conseguiuEditar = repositorioPaciente.Editar(editarVm.Id, pacienteAtualizado);
+
+        if (!conseguiuEditar)
+            return NotFound();
+
+        return RedirectToAction(nameof(Listar));
+    }
+
 }
--- ControleDeMedicamentos.ConsoleApp/ModuloPaciente/PacienteViewModels.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPaciente/PacienteViewModels.cs
@@ -1,3 +1,8 @@
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
 
 public record ListarPacienteViewModel(int Id, string Nome, string Telefone, string CartaoSus);
+
+public record CadastrarPacienteViewModel(string Nome, string Telefone, string CartaoSus, string CPF);
+
+public record EditarPacienteViewModel(int Id, string Nome, string Telefone, string CartaoSus, string CPF);
+public record ExcluirPacienteViewModel(int Id, string Nome);
